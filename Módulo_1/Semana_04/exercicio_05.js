0958a56661e7d10c21c2f4a8b2530f0d014f1ca1
Conrado Fortunato <conradofortunato246@gmail.com>
2022-03-24 10:43:15 -0300

    feat: add 'exercicio_05.js' file

---
+++ Módulo_1/Semana_04/exercicio_05.js
@@ -0,0 +1,13 @@
+var animais = [
+    { animal: 'Cachorro', raca: 'Akita' },
+    { animal: 'Cachorro', raca: 'Boxer' },
+    { animal: 'Cachorro', raca: 'Dálmata' },
+    { animal: 'Cachorro', raca: 'Pastor alemão' },
+    { animal: 'Gato', raca: 'Persa' },
+    { animal: 'Gato', raca: 'Sphynx' },
+    { animal: 'Gato', raca: 'Siamês' },
+]
+
+animais.forEach((item) => {
+    console.log(`Um ${item.animal} da raça ${item.raca}`)
+})
