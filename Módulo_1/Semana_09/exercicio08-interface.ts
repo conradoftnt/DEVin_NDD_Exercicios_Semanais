944f43c103ff566eb23890c3ddc312f39f7264da
Conrado Fortunato <conradofortunato246@gmail.com>
2022-05-03 14:19:48 -0300

    feat: exercice 08 complete

---
+++ Módulo_1/Semana_09/exercicio08-interface.ts
@@ -0,0 +1,6 @@
+export interface Pessoa {
+    nome: string;
+    idade: number;
+    rua: string;
+    cadastrar(): void;
+}
---
+++ Módulo_1/Semana_09/exercicio08.ts
@@ -0,0 +1,52 @@
+import { Pessoa } from "./exercicio08-interface";
+
+class Aluno implements Pessoa {
+    nome: string;
+    idade: number;
+    rua: string;
+    matricula: number;
+    cadeiras: string[];
+    constructor(
+        nome: string,
+        idade: number,
+        rua: string,
+        matricula: number,
+        cadeiras: string[],
+    ) {
+        this.nome = nome;
+        this.idade = idade;
+        this.rua = rua;
+        this.matricula = matricula;
+        this.cadeiras = cadeiras;
+    }
+    cadastrar(): void {
+        console.log(this);
+    }
+}
+
+class Funcionario implements Pessoa {
+    nome: string;
+    idade: number;
+    rua: string;
+    identificador: number;
+    setor: string;
+    constructor(
+        nome: string,
+        idade: number,
+        rua: string,
+        identificador: number,
+        setor: string,
+    ) {
+        this.nome = nome;
+        this.idade = idade;
+        this.rua = rua;
+        this.identificador = identificador;
+        this.setor = setor;
+    }
+    cadastrar(): void {
+        console.log(this);
+    }
+}
+
+let teste = new Aluno("a", 1, "a", 1, ["a"]);
+console.log(teste);
