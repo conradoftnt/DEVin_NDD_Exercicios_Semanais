9dd48d305703138af76161ee927d95467a0c2946
Conrado Fortunato <conradofortunato246@gmail.com>
2022-05-03 13:07:32 -0300

    feat: exercice 04 complete

---
+++ Módulo_1/Semana_09/exercicio04.ts
@@ -0,0 +1,11 @@
+class Filme {
+    nome: string;
+    anoDeLancamento: number;
+    diretor: string;
+}
+
+let meuFilme = new Filme();
+
+meuFilme.nome = "Brother Bear";
+meuFilme.anoDeLancamento = 2003;
+meuFilme.diretor = "Aaron Blaise, Robert Walker";
