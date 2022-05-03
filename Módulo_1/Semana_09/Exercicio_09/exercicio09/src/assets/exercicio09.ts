2ab27be6183bfec62b664bf23bdde402b4186ca2
Conrado Fortunato <conradofortunato246@gmail.com>
2022-05-03 17:30:51 -0300

    feat: exercice09 complete

---
+++ Módulo_1/Semana_09/Exercicio_09/exercicio09/src/assets/exercicio09.ts
@@ -0,0 +1,8 @@
+export interface Produto {
+  nome: string;
+  valor: number;
+  codigo: number;
+  quantidade: number;
+  emEstoque: boolean;
+  comprar?(produto: Produto, quantidade: number): any;
+}
