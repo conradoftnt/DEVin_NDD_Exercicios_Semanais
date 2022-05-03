1b22502c0da9edc35fca5dd91363f90d11a1404b
Conrado Fortunato <conradofortunato246@gmail.com>
2022-05-03 11:23:35 -0300

    feat: exercice 01 complete

---
+++ Módulo_1/Semana_09/exercicio01.ts
@@ -0,0 +1,4 @@
+let exampleLet: Number = 21;
+const exampleConst: String = "Conrado Fortunato";
+var exempleVar: String =
+    "Olá meu nome é " + exampleConst + " e tenho " + exampleLet + " anos.";
