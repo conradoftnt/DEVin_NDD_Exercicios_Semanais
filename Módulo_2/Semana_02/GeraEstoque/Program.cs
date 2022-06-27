fe04d35cda96aa61a0d3954fdb008a0d636e0f31
Conrado Fortunato <conradofortunato246@gmail.com>
2022-06-27 15:49:06 -0300

    feat: Exercice 06 complete

--- Módulo_2/Semana_02/GeraEstoque/Program.cs
+++ Módulo_2/Semana_02/GeraEstoque/Program.cs
@@ -3,6 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Seja bem vindo");
+        Console.Write("Seja bem vindo ao GeraEstoque 1.0\n----------------------------------------\n1  Cadastrar produto\n2  Consultar produto\n3  Modificar produto\n4  Excluir produto\n0  Sair\n\nDigite a opção:");
     }
 }
