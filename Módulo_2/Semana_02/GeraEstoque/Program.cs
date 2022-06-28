6f67148b6e7493fc29b0afcd9862da76d0910fe2
Conrado Fortunato <conradofortunato246@gmail.com>
2022-06-28 20:18:53 -0300

    feat: Exercice 07 complete

--- Módulo_2/Semana_02/GeraEstoque/Program.cs
+++ Módulo_2/Semana_02/GeraEstoque/Program.cs
@@ -4,6 +4,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Seja bem vindo");
-        Console.Write("Seja bem vindo ao GeraEstoque 1.0\n----------------------------------------\n1  Cadastrar produto\n2  Consultar produto\n3  Modificar produto\n4  Excluir produto\n0  Sair\n\nDigite a opção:");
+
+        Console.Write(@"Seja bem vindo ao GeraEstoque 1.0
+----------------------------------------
+1  Cadastrar produto
+2  Consultar produto
+3  Modificar produto
+4  Excluir produto
+0  Sair
+
+Digite a opção: ");
+
+        var option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                CadastrarProduto();
+                break;
+
+            default:
+                Console.Clear();
+                break;
+        }
+    }
+
+    static void CadastrarProduto()
+    {
     }
 }
