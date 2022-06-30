bccd0f1d74f5a1b4573a79694b9ab025d51f1028
Conrado Fortunato <conradofortunato246@gmail.com>
2022-06-29 21:02:38 -0300

    feat: Exercice 04 complete

--- Módulo_2/Semana_03/GeraEstoque/Screens/CriarProdutoScreen.cs
+++ Módulo_2/Semana_03/GeraEstoque/Screens/CriarProdutoScreen.cs
@@ -2,6 +2,35 @@ namespace GeraEstoque.Screens
 {
     public static class CriarProdutoScreen
     {
+        public static void Iniciar()
+        {
 
+            Console.Write("Digite o nome do produto a ser cadastrado: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite quantas unidades do produto estão disponíveis no estoque: ");
+            int qntEstoque = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da compra do produto: ");
+            float vlrCompra = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de venda do produto: ");
+            float vlrVenda = float.Parse(Console.ReadLine());
+
+            Guid productId = Guid.NewGuid();
+
+            Console.WriteLine(@$"Produto cadastrado com sucesso!
+
+ID: {productId};
+Nome: Produto {nome}; 
+Qtd: {qntEstoque};
+R$ Compra: R$ {vlrCompra};
+R$ Venda: {vlrVenda};”.");
+
+            Console.Write(Environment.NewLine);
+            Console.Write("Pressione qualquer tecla para voltar ao menu principal.");
+            Console.ReadKey();
+            Console.Write(Environment.NewLine);
+        }
     }
 }
--- Módulo_2/Semana_03/GeraEstoque/Screens/MenuScreen.cs
+++ Módulo_2/Semana_03/GeraEstoque/Screens/MenuScreen.cs
@@ -25,7 +25,8 @@ Digite a opção: ");
             switch (opcao)
             {
                 case "1":
-                    CadastrarProduto();
+                    CriarProdutoScreen.Iniciar();
+                    Iniciar();
                     break;
 
                 case "2":
@@ -49,37 +50,6 @@ Digite a opção: ");
                     Iniciar();
                     break;
             }
-        }
-
-        static void CadastrarProduto()
-        {
-            Console.Write("Digite o nome do produto a ser cadastrado: ");
-            string nome = Console.ReadLine();
-
-            Console.Write("Digite quantas unidades do produto estão disponíveis no estoque: ");
-            int qntEstoque = int.Parse(Console.ReadLine());
-
-            Console.Write("Digite o valor da compra do produto: ");
-            float vlrCompra = float.Parse(Console.ReadLine());
-
-            Console.Write("Digite o valor de venda do produto: ");
-            float vlrVenda = float.Parse(Console.ReadLine());
-
-            Guid productId = Guid.NewGuid();
-
-            Console.WriteLine(@$"Produto cadastrado com sucesso!
-
-ID: {productId};
-Nome: Produto {nome}; 
-Qtd: {qntEstoque};
-R$ Compra: R$ {vlrCompra};
-R$ Venda: {vlrVenda};”.");
-
-            Console.Write(Environment.NewLine);
-            Console.Write("Pressione qualquer tecla para voltar ao menu principal.");
-            Console.ReadKey();
-            Console.Write(Environment.NewLine);
-            Iniciar();
         }
     }
 }
