14659056a4963bf26117dce18833275527fa9600
Conrado Fortunato <conradofortunato246@gmail.com>
2022-07-11 04:13:24 -0300

    feat: Exercices 07 and 08 complete

--- Módulo_2/Semana_04/Models/CobrarCurso.cs
+++ Módulo_2/Semana_04/Models/CobrarCurso.cs
@@ -5,4 +5,26 @@ public class CobrarCurso
     private float ValorCurso;
     private float ValorDesconto;
     private float ValorMulta;
+    public float Resultado;
+
+    CobrarCurso(FichaInscricao Usuario)
+    {
+        ValorCurso = Usuario.ValorCurso;
+        ValorDesconto = Usuario.ValorDesconto;
+        ValorMulta = Usuario.ValorMulta;
+
+        Calculo();
+    }
+
+    private void Calculo()
+    {
+        if (ValorMulta > 0)
+        {
+            Resultado = ValorCurso + ValorMulta;
+        }
+        else
+        {
+            Resultado = ValorCurso - ValorDesconto;
+        }
+    }
 }
