e76ae09e6973d6756d882f3214ccd5e7ac809605
Conrado Fortunato <conradofortunato246@gmail.com>
2022-07-11 01:25:06 -0300

    feat: Exercice 01 complete

---
+++ Módulo_2/Semana_04/Models/FichaInscricao.cs
@@ -0,0 +1,13 @@
+namespace Semana_04.Models;
+
+class FichaInscricao
+{
+    public string? Nome;
+    public int Idade;
+    public string? Curso;
+    public string? Escolaridade;
+    public DateOnly DataNascimento;
+    public float ValorCurso;
+    public float ValorDesconto;
+    public float ValorMulta;
+}
---
+++ Módulo_2/Semana_04/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Semana_04;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
