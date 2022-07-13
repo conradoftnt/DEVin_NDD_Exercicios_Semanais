8522bcdc074a73d33dd31d47e77267707972c9ab
Conrado Fortunato <conradofortunato246@gmail.com>
2022-07-13 05:30:58 -0300

    feat: Exercice_05 compelte

---
+++ Módulo_2/Semana_05/Ex_05_06/Models/Funcionario.cs
@@ -0,0 +1,13 @@
+namespace Ex_05_06.Models;
+
+public class Funcionario
+{
+    public string Nome;
+    public string Id;
+
+    public Funcionario(string nome, string id)
+    {
+        Nome = nome;
+        Id = id;
+    }
+}
---
+++ Módulo_2/Semana_05/Ex_05_06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex_05_06;
+using Ex_05_06.Models;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Funcionario funcionario = new Funcionario("Teste Funcionario", "00001");
+
+        VerificaNulo(funcionario);
+
+        void VerificaNulo(Funcionario Object)
+        {
+            try
+            {
+                if (Object is not null)
+                {
+                    Console.WriteLine(Object.Nome);
+                }
+            }
+            catch (Exception Erro)
+            {
+                throw new Exception(Erro.ToString());
+            }
+        }
+    }
+}
