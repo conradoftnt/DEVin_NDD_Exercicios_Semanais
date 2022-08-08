e401e49c018c5eb9355fbaeea17a008441af4345
Conrado Fortunato <conradofortunato246@gmail.com>
2022-08-08 07:26:45 -0300

    fix: Exercices 05, 06 and 07

--- Módulo_2/Semana_08/Semana_08/Exercicio_05.sql
+++ Módulo_2/Semana_08/Semana_08/Exercicio_05.sql
@@ -1,11 +1,14 @@
 CREATE DATABASE Empresa;
+GO
 
 USE Empresa;
+GO
 
 CREATE TABLE Cargos (
 	id_cargo int PRIMARY KEY IDENTITY (1,1),
 	nome_cargo VARCHAR(200) NOT NULL
 );
+GO
 
 CREATE TABLE Gestores (
 	id_gestor int PRIMARY KEY IDENTITY (1,1),
@@ -13,6 +16,7 @@ CREATE TABLE Gestores (
 	cargo_id int NOT NULL,
 	CONSTRAINT fk_Gestor_Cargo FOREIGN KEY (cargo_id) REFERENCES Cargos(id_cargo)
 );
+GO
 
 CREATE TABLE Empregados (
 	id_emp int PRIMARY KEY IDENTITY (1,1), 
@@ -24,3 +28,4 @@ CREATE TABLE Empregados (
 	CONSTRAINT fk_Empregados_Cargos FOREIGN KEY (cargo_id) REFERENCES Cargos(id_cargo),
 	CONSTRAINT fk_Gestor_Empregado FOREIGN KEY (gestor_id) REFERENCES Gestores(id_gestor)
 );
+GO
