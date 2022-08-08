c6482397333325d68241ae36d8f806db243f8ced
Conrado Fortunato <conradofortunato246@gmail.com>
2022-08-08 06:51:43 -0300

    fix: Exercice 06

--- Módulo_2/Semana_08/Semana_08/Exercicio_06.sql
+++ Módulo_2/Semana_08/Semana_08/Exercicio_06.sql
@@ -1,91 +1,40 @@
 USE Empresa;
 
 INSERT INTO Cargos
-VALUES ('Dono');
-
-INSERT INTO Cargos
-VALUES ('Diretor');
-
-INSERT INTO Cargos
-VALUES ('Chefe');
-
-INSERT INTO Cargos
-VALUES ('Analista');
-
-INSERT INTO Cargos
-VALUES ('Secretario');
-
-INSERT INTO Cargos
-VALUES ('Especialista');
-
-INSERT INTO Cargos
-VALUES ('Vendedor');
-
-INSERT INTO Cargos
-VALUES ('Contador');
-
-INSERT INTO Cargos
-VALUES ('Zelador');
-
-INSERT INTO Cargos
-VALUES ('Estagiario');
+VALUES 
+	('Dono'),
+	('Diretor'),
+	('Chefe'),
+	('Analista'),
+	('Secretario'),
+	('Especialista'),
+	('Vendedor'),
+	('Contador'),
+	('Zelador'),
+	('Estagiario');
 
 INSERT INTO Gestores
-VALUES ('Adriana', 2);
-
-INSERT INTO Gestores
-VALUES ('Eduardo', 10);
-
-INSERT INTO Gestores
-VALUES ('Cleber', 7);
-
-INSERT INTO Gestores
-VALUES ('Ana', 1);
-
-INSERT INTO Gestores
-VALUES ('Maria', 5);
-
-INSERT INTO Gestores
-VALUES ('Denise', 8);
-
-INSERT INTO Gestores
-VALUES ('João', 4);
-
-INSERT INTO Gestores
-VALUES ('Lucas', 6);
-
-INSERT INTO Gestores
-VALUES ('Pablo', 9);
-
-INSERT INTO Gestores
-VALUES ('Gustavo', 3);
+VALUES 
+	('Adriana', 2),
+	('Eduardo', 10),
+	('Cleber', 7),
+	('Ana', 1),
+	('Maria', 5),
+	('Denise', 8),
+	('João', 4),
+	('Lucas', 6),
+	('Pablo', 9),
+	('Gustavo', 3);
 
 INSERT INTO Empregados
-VALUES ('Adriana', 'Rua Certa', 23, 2, 1);
-
-INSERT INTO Empregados
-VALUES ('Gustavo', 'Rua Errada', 27, 3, 2);
-
-INSERT INTO Empregados
-VALUES ('Denise', 'Rua Indeterminada', 35, 8, 3);
-
-INSERT INTO Empregados
-VALUES ('Pablo', 'Rua Sem Saida', 46, 9, 3);
-
-INSERT INTO Empregados
-VALUES ('Eduardo', 'Rua Com Saida', 22, 10, 3);
-
-INSERT INTO Empregados
-VALUES ('Cleber', 'Rua Não Registrada', 39, 7, 3);
-
-INSERT INTO Empregados
-VALUES ('Otavio', 'Rua Registrada', 17, 10, 3);
-
-INSERT INTO Empregados
-VALUES ('João', 'Rua Azul', 27, 4, 2);
-
-INSERT INTO Empregados
-VALUES ('Maria', 'Rua Verde', 18, 5, 2);
-
-INSERT INTO Empregados
-VALUES ('Lucas', 'Rua Amarela', 26, 6, 2);
+VALUES 
+	('Adriana', 'Rua Certa', 23, 2, 1),
+	('Gustavo', 'Rua Errada', 27, 3, 2),
+	('Denise', 'Rua Indeterminada', 35, 8, 3),
+	('Pablo', 'Rua Sem Saida', 46, 9, 3),
+	('Eduardo', 'Rua Com Saida', 22, 10, 3),
+	('Cleber', 'Rua Não Registrada', 39, 7, 3),
+	('Otavio', 'Rua Registrada', 17, 10, 3),
+	('João', 'Rua Azul', 27, 4, 2),
+	('Maria', 'Rua Verde', 18, 5, 2),
+	('Lucas', 'Rua Amarela', 26, 6, 2);
